b8f5e81722e527db6c44897edc3a0ee445b7e141
riofchrdn <riofchrdn@gmail.com>
2023-03-10 06:55:52 +0700

    update view

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -6,6 +6,10 @@ class Home extends BaseController
 {
     public function index()
     {
+        echo view('templates/v_header');
+        echo view('templates/v_sidebar');
+        echo view('templates/v_topbar');
         echo view('home/index');
+        echo view('templates/v_footer');
     }
 }
